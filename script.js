f461c024a5bb239256459fc981b7fa27e1ca506f
PathumSanjana <silvakp-se19030@stu.kln.ac.lk>
2023-08-14 13:41:16 +0530

    project section updated

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ function toggleMenu() {
     checkbox.checked = !checkbox.checked;
 }
 
+function scrollToSection(sectionId) {
+    const section = document.querySelector(sectionId);
+    if (section) {
+        const offsetTop = section.offsetTop;
+        window.scrollTo({
+            top: offsetTop - (window.innerHeight - section.clientHeight) / 2,
+            behavior: 'smooth'
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            const targetSectionId = link.getAttribute('href');
+            scrollToSection(targetSectionId);
+        });
+    });
+});
+
+
+
